53c6cf1621e53fb45b02e47a96fcd84ae84915b3
Rosmery-CC <1002820232@unajma.edu.pe>
2025-11-13 00:38:12 -0500

    Iniciar setup - Angular + Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAvAuCqFLjMwS8_Wm-k6faTTeyweaH5C9A",
+    authDomain: "sistema-de-tickets-ec231.firebaseapp.com",
+    projectId: "sistema-de-tickets-ec231",
+    storageBucket: "sistema-de-tickets-ec231.firebasestorage.app",
+    messagingSenderId: "1092051798727",
+    appId: "1:1092051798727:web:a2ae24e4b034c339e44ce2"
+  }
+};
